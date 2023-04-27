2b66a02c6d0039c873d4f9ea052a3bdba0c4f97e
xxhmiguelxx <125937690+xxhmiguelxx@users.noreply.github.com>
2023-04-26 22:03:41 -0600

    Add files via upload

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,67 +1,12 @@
-public class Vehiculo {
+public interface Vehiculo {
 
-    protected int numRuedas;
-    protected String combustible;
-    protected String motor;
-    protected int numPuertas;
+    public static final int VELOCIDAD_MAXIMA=160;
+    public static final char CLASIFIACION_EFICIENCIA= 'A';
 
-    public Vehiculo() {
-    }
 
-    public Vehiculo(int numRuedas, String combustible, String motor, int numPuertas) {
-        this.numRuedas = numRuedas;
-        this.combustible = combustible;
-        this.motor = motor;
-        this.numPuertas = numPuertas;
-    }
+    public boolean encender();
+    public boolean apagar();
+    public void avanzar();
+    public void frenar();
 
-    public int getNumRuedas() {
-        return numRuedas;
-    }
-
-    public void setNumRuedas(int numRuedas) {
-        this.numRuedas = numRuedas;
-    }
-
-    public String getCombustible() {
-        return combustible;
-    }
-
-    public void setCombustible(String combustible) {
-        this.combustible = combustible;
-    }
-
-    public String getMotor() {
-        return motor;
-    }
-
-    public void setMotor(String motor) {
-        this.motor = motor;
-    }
-
-    public int getNumPuertas() {
-        return numPuertas;
-    }
-
-    public void setNumPuertas(int numPuertas) {
-        this.numPuertas = numPuertas;
-    }
-
-    @Override
-    public String toString() {
-        return "Vehiculo{" +
-                "numRuedas=" + numRuedas +
-                ", combustible='" + combustible + '\'' +
-                ", motor='" + motor + '\'' +
-                ", numPuertas=" + numPuertas +
-                '}';
-    }
-
-    public void encender() {
-        System.out.println("Encendiendo...");
-    }
-
-    public void apagar() {
-        System.out.println("Apagando...");
-    }
 }
